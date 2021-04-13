bc8e9819ca54222b09be21a119a0da3132c58031
DESKTOP-RDD88MA\LG <tjsdk2486@gmil.com>
2021-04-13 19:39:28 +0900

    특별예제3

---
+++ 특별문제1-3/특별문제1-3/Source.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() {
+
+	for (int a = 0; a < 4; a++) {
+		printf("*\n");
+		for (int b = 0; b < a + 2; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}    fgetc(stdin);
+}
